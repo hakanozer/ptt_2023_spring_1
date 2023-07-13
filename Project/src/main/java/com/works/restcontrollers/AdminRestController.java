015c0a28ed100f90b4900ca634e1c66bc3ff0f23
Hakan ÖZER <hakanozer02@gmail.com>
2023-07-13 12:27:57 +0300

    Session Create

---
+++ Project/src/main/java/com/works/restcontrollers/AdminRestController.java
@@ -0,0 +1,29 @@
+package com.works.restcontrollers;
+
+import com.works.entities.Admin;
+import com.works.services.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminRestController {
+
+    final AdminService adminService;
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody Admin admin) {
+        return adminService.register(admin);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody Admin admin) {
+        return adminService.login(admin);
+    }
+
+}
--- Project/src/main/java/com/works/services/AdminService.java
+++ Project/src/main/java/com/works/services/AdminService.java
@@ -8,12 +8,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class AdminService {
 
     final AdminRepository adminRepository;
     final TinkEncDec tinkEncDec;
+    final HttpServletRequest req;
 
     public ResponseEntity register( Admin admin ) {
         try {
@@ -23,6 +28,19 @@ public class AdminService {
         }catch (Exception ex) {
             return Rest.fail(admin, HttpStatus.BAD_REQUEST);
         }
+    }
+
+    public ResponseEntity login( Admin admin ) {
+        Optional<Admin> optionalAdmin = adminRepository.findByUsernameEqualsIgnoreCase(admin.getUsername());
+        if (optionalAdmin.isPresent()) {
+            Admin adm = optionalAdmin.get();
+            String dbPass = tinkEncDec.decrypt(adm.getPassword());
+            if ( dbPass.equals(admin.getPassword()) ) {
+                req.getSession().setAttribute("admin", adm);
+                return Rest.success(adm);
+            }
+        }
+        return Rest.fail("Username or Password Fail", HttpStatus.BAD_REQUEST);
     }
 
 }
